311d0a1f874537228c19772306caad3abc65a943
ECS Admin <ecsadmin@ECSs-Mac-Pro.byu.edu>
2024-01-19 15:23:37 -0700

    fermat project first attempt

--- projects/project1-fermat/fermat.py
+++ projects/project1-fermat/fermat.py
@@ -4,32 +4,50 @@ import random
 def prime_test(N, k):
 	return fermat(N,k), miller_rabin(N,k)
 
-# You will need to implement this function and change the return value.
 def mod_exp(x, y, N): 
-	return 1
+	if y == 0:
+		return 1
+	z = mod_exp(x,y//2,N)
+	if y%2 == 0:
+		return (z**2) % N
+	else:
+		return (x*(z**2))% N
 	
-# You will need to implement this function and change the return value.   
+  
 def fprobability(k):
-    return 0.0
+    return 1 - (0.5 ** k)
 
 # You will need to implement this function and change the return value.   
 def mprobability(k):
     return 0.0
 
-# You will need to implement this function and change the return value, which should be
-# either 'prime' or 'composite'.
-#
-# To generate random values for a, you will most likley want to use
-# random.randint(low,hi) which gives a random integer between low and
-# hi, inclusive.
+
 def fermat(N,k):
+	low = 2
+	hi = N-1
+	for i in range(k):
+		a = random.randint(low,hi)
+		mod_result = mod_exp(a,N-1,N)
+		if mod_result != 1:
+			return 'composite'
 	return 'prime'
 
-# You will need to implement this function and change the return value, which should be
-# either 'prime' or 'composite'.
-#
-# To generate random values for a, you will most likley want to use
-# random.randint(low,hi) which gives a random integer between low and
-#  hi, inclusive.
+
 def miller_rabin(N,k):
-	return 'composite'
+	low = 2
+	hi = N-1
+	for i in range(k):
+		a = random.randint(low,hi)
+		exponent = N-1
+		while True:
+			mod_result = mod_exp(a,exponent,N)
+			if mod_result != 1 and mod_result != N-1:
+				return 'composite'
+			if (exponent)%2 == 0:
+				exponent = int(exponent*0.5)
+			else: 
+				break
+			if mod_result == N-1:
+				break
+	print("returning prime")
+	return 'prime'
