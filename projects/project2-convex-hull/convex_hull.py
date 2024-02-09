d6a7ef4ecabff2e92a84ce8d5867cce59cce471b
Max Forsey <maxforsey@Max-Forsey.byu.edu>
2024-02-09 16:33:56 -0700

    just need to fix lower tangent now

--- projects/project2-convex-hull/convex_hull.py
+++ projects/project2-convex-hull/convex_hull.py
@@ -61,43 +61,49 @@ class ConvexHullSolver(QObject):
     def top_clock_walk(self,r_point,l_point,r_points,l_points):
         compare_point_l = l_point
         compare_point_r = r_point
-        i = r_points.index(r_point)
-        j = l_points.index(l_point)
+        i = r_points.index(r_point) # r_index
+        j = l_points.index(l_point) # l_index
         change_in_r = True
         change_in_l = True
 
-        while change_in_r and change_in_l: 
+        while change_in_r or change_in_l: 
             change_in_r = False
             change_in_l = False
 
-            r_point_1 = r_points[i]
-            r_point_2 = r_points[(i + 1) % len(r_points)]
+            while True:
+                r_point_1 = r_points[i]
+                r_point_2 = r_points[(i + 1) % len(r_points)]
 
-            deltaY, deltaX = r_point_1.y() - compare_point_l.y(), r_point_1.x() - compare_point_l.x()
-            slope_1 = deltaY/deltaX   
+                deltaY, deltaX = r_point_1.y() - compare_point_l.y(), r_point_1.x() - compare_point_l.x()
+                slope_1 = deltaY/deltaX   
 
-            deltaY, deltaX = r_point_2.y() - compare_point_l.y(), r_point_2.x() - compare_point_l.x()
-            slope_2 = deltaY/deltaX 
+                deltaY, deltaX = r_point_2.y() - compare_point_l.y(), r_point_2.x() - compare_point_l.x()
+                slope_2 = deltaY/deltaX 
 
-            if slope_1 < slope_2:
-                i = (i + 1) % len(r_points)
-                compare_point_r = r_point_2
-                change_in_r = True
+                if slope_1 < slope_2:
+                    i = (i + 1) % len(r_points)
+                    compare_point_r = r_point_2
+                    change_in_r = True
+                else:
+                    break
 
-            #counter clockwise        
-            l_point_1 = l_points[(j) % len(l_points)]
-            l_point_2 = l_points[(j - 1) % len(l_points)]
+            #counter clockwise
+            while True:        
+                l_point_1 = l_points[(j) % len(l_points)]
+                l_point_2 = l_points[(j - 1) % len(l_points)]
 
-            deltaY, deltaX = l_point_1.y() - compare_point_r.y(), l_point_1.x() - compare_point_r.x()
-            slope_1 = deltaY/deltaX  
+                deltaY, deltaX = l_point_1.y() - compare_point_r.y(), l_point_1.x() - compare_point_r.x()
+                slope_1 = deltaY/deltaX  
 
-            deltaY, deltaX = l_point_2.y() - compare_point_r.y(), l_point_2.x() - compare_point_r.x()
-            slope_2 = deltaY/deltaX 
+                deltaY, deltaX = l_point_2.y() - compare_point_r.y(), l_point_2.x() - compare_point_r.x()
+                slope_2 = deltaY/deltaX 
 
-            if slope_1 > slope_2:
-                j = (j - 1) % len(l_points)
-                compare_point_l = l_point_2
-                change_in_l = True
+                if slope_1 > slope_2:
+                    j = (j - 1) % len(l_points)
+                    compare_point_l = l_point_2
+                    change_in_l = True
+                else:
+                    break
                 
         return compare_point_r, compare_point_l
 
@@ -107,83 +113,50 @@ class ConvexHullSolver(QObject):
         compare_point_r = r_point
         i = r_points.index(r_point)
         j = l_points.index(l_point)
+        change_in_r = True
+        change_in_l = True
 
-        while True:
+        while change_in_r or change_in_l:
             change_in_r = False
             change_in_l = False
 
-            r_point_1 = r_points[i]
-            r_point_2 = r_points[(i - 1) % len(r_points)]
+            while True:   
+                r_point_1 = r_points[i]
+                r_point_2 = r_points[(i - 1) % len(r_points)]
 
-            deltaY, deltaX = r_point_1.y() - compare_point_l.y(), r_point_1.x() - compare_point_l.x()
-            slope_1 = deltaY/deltaX 
+                deltaY, deltaX = r_point_1.y() - compare_point_l.y(), r_point_1.x() - compare_point_l.x()
+                slope_1 = deltaY/deltaX 
 
-            deltaY, deltaX = r_point_2.y() - compare_point_l.y(), r_point_2.x() - compare_point_l.x()
-            slope_2 = deltaY/deltaX
+                deltaY, deltaX = r_point_2.y() - compare_point_l.y(), r_point_2.x() - compare_point_l.x()
+                slope_2 = deltaY/deltaX
 
-            if slope_1 > slope_2:
-                i = (i - 1) % len(r_points)
-                compare_point_r = r_point_2
-                change_in_r = True
-                    
-            l_point_1 = l_points[(j) % len(l_points)]
-            l_point_2 = l_points[(j + 1) % len(l_points)]
+                if slope_1 > slope_2:
+                    i = (i - 1) % len(r_points)
+                    compare_point_r = r_point_2
+                    change_in_r = True
+                else:
+                    break
+            
+            while True:   
+                l_point_1 = l_points[(j) % len(l_points)]
+                l_point_2 = l_points[(j + 1) % len(l_points)]
 
-            deltaY, deltaX = l_point_1.y() - compare_point_r.y(), l_point_1.x() - compare_point_r.x()
-            slope_1 = deltaY/deltaX 
+                deltaY, deltaX = l_point_1.y() - compare_point_r.y(), l_point_1.x() - compare_point_r.x()
+                slope_1 = deltaY/deltaX 
 
-            deltaY, deltaX = l_point_2.y() - compare_point_r.y(), l_point_2.x() - compare_point_r.x()
-            slope_2 = deltaY/deltaX
+                deltaY, deltaX = l_point_2.y() - compare_point_r.y(), l_point_2.x() - compare_point_r.x()
+                slope_2 = deltaY/deltaX
 
-            if slope_1 < slope_2:
-                j = (j + 1) % len(l_points)
-                compare_point_l = l_point_2
-                change_in_l = True
+                if slope_1 < slope_2:
+                    j = (j + 1) % len(l_points)
+                    compare_point_l = l_point_2
+                    change_in_l = True
+                else:
+                    break
                 
-            if not change_in_r and not change_in_l:
-                break
 
         return compare_point_r, compare_point_l
 
-
-    def add_right_side(self,top_point,low_point,points):
-        #top to bottom
-        side_points = []
-
-        if top_point != low_point:
-            i = points.index(top_point)
-            while True:
-                point_1 = points[i]
-                point_2 = points[(i + 1) % len(points)]
-                side_points.append(point_1)
-                side_points.append(point_2)
-                if(point_2 == low_point):
-                    break
-                else:
-                    i = (i + 1) % len(points)
-        else:
-            side_points.append(top_point)
- 
-        return side_points
-    
-    def add_left_side(self,top_point,low_point,points,side_points):
-        # bottom to top
-        
-        if top_point != low_point:
-            i = points.index(low_point)
-            while True:
-                point_1 = points[i]
-                point_2 = points[(i + 1) % len(points)]
-                side_points.append(point_1)
-                side_points.append(point_2)
-                if(point_2 == top_point):
-                    break
-                else:
-                    i = (i + 1) % len(points)
-        else:
-            side_points.append(top_point)
- 
-        return side_points
 
     def combine(self,l_points,r_points):
 
@@ -191,15 +164,24 @@ class ConvexHullSolver(QObject):
         r_point = r_points[0] #leftmost point in the r_hull
 
         top_r,top_l = self.top_clock_walk(r_point,l_point,r_points,l_points)
-        #lines = []
-        #lines.append(QLineF(top_r,top_l))
         low_r,low_l = self.low_clock_walk(r_point,l_point,r_points,l_points)
-        #lines.append(QLineF(low_r,low_l))
-        #self.blinkTangent(lines,RED)
 
-        # must be Tl, TR, BR, BL  in clockwise order
-        points = self.add_right_side(top_r,low_r,r_points)
-        points = self.add_left_side(top_l,low_l,l_points,points)
+        top_r_index = r_points.index(top_r)
+        top_l_index = l_points.index(top_l)
+        low_r_index = r_points.index(low_r)
+        low_l_index = l_points.index(low_l)
+
+
+        # must be Tl, TR, BR, BL in clockwise order starting with the leftmost point
+
+        #points = self.add_top_left_side(l_points[0],top_l,l_points)
+        points = l_points[:top_l_index+1]
+        if low_r_index > top_r_index:
+            points = points + r_points[top_r_index:low_r_index+1]
+        else:
+            points = points + r_points[top_r_index:] + r_points[:low_r_index+1] 
+        if low_l_index != 0:
+            points = points + l_points[low_l_index:]
 
         return points
     
@@ -242,7 +224,7 @@ class ConvexHullSolver(QObject):
         assert( type(points) == list and type(points[0]) == QPointF )
 
         # Time Complexity: O(n log n)
-        points = [QPointF(0.3052037135928627, -0.16678789881668887), QPointF(0.36054318841709554, -0.022101145365918562), QPointF(0.4644680423427776, -0.484935002648909), QPointF(0.24994208445361088, -0.5629500170601016), QPointF(-0.39968552992684736, 0.5564877948800708)]
+        #points = [QPointF(-0.06497354041572767, 0.020731865290767093),QPointF(0.2589470988470015, 0.49210390954117633), QPointF(0.7802170655842446, -0.27517740152568715), QPointF(-0.12090869034040619, -0.4024538576552268), QPointF(-0.7576767678606016, 0.4148990056214117)]
         points.sort(key=lambda point: point.x())
 
         t2 = time.time()
@@ -250,7 +232,7 @@ class ConvexHullSolver(QObject):
         t3 = time.time()
 
         polygon_points = self.divide_and_conquer(points)
-        print(polygon_points)
+        #print(polygon_points)
         polygon = [QLineF(polygon_points[i], polygon_points[(i+1) % len(polygon_points)]) for i in range(len(polygon_points))]
 
 
