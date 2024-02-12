c885330ec7b62d64d32450b669b0b13adcdfe48e
Max Forsey <maxforsey@Max-Forsey.byu.edu>
2024-02-12 16:53:46 -0700

    solved issue when top and bottom tangent point the same

--- projects/project2-convex-hull/convex_hull.py
+++ projects/project2-convex-hull/convex_hull.py
@@ -66,7 +66,7 @@ class ConvexHullSolver(QObject):
         change_in_r = True
         change_in_l = True
 
-        while change_in_r or change_in_l: 
+        while change_in_r or change_in_l:
             change_in_r = False
             change_in_l = False
 
@@ -136,7 +136,8 @@ class ConvexHullSolver(QObject):
                     change_in_r = True
                 else:
                     break
-            
+
+            #clockwise
             while True:   
                 l_point_1 = l_points[(j) % len(l_points)]
                 l_point_2 = l_points[(j + 1) % len(l_points)]
@@ -167,17 +168,16 @@ class ConvexHullSolver(QObject):
         low_r,low_l = self.low_clock_walk(r_point,l_point,r_points,l_points)
 
         top_r_index = r_points.index(top_r)
-        top_l_index = l_points.index(top_l)
         low_r_index = r_points.index(low_r)
+        top_l_index = l_points.index(top_l)
         low_l_index = l_points.index(low_l)
 
-
         # must be Tl, TR, BR, BL in clockwise order starting with the leftmost point
-
-        #points = self.add_top_left_side(l_points[0],top_l,l_points)
         points = l_points[:top_l_index+1]
         if low_r_index > top_r_index:
             points = points + r_points[top_r_index:low_r_index+1]
+        elif low_r_index == top_r_index:
+                points.append(r_points[low_r_index])
         else:
             points = points + r_points[top_r_index:] + r_points[:low_r_index+1] 
         if low_l_index != 0:
@@ -223,23 +223,17 @@ class ConvexHullSolver(QObject):
         self.view = view
         assert( type(points) == list and type(points[0]) == QPointF )
 
+        t1 = time.time()
+
         # Time Complexity: O(n log n)
-        #points = [QPointF(-0.06497354041572767, 0.020731865290767093),QPointF(0.2589470988470015, 0.49210390954117633), QPointF(0.7802170655842446, -0.27517740152568715), QPointF(-0.12090869034040619, -0.4024538576552268), QPointF(-0.7576767678606016, 0.4148990056214117)]
         points.sort(key=lambda point: point.x())
-
-        t2 = time.time()
-
-        t3 = time.time()
-
         polygon_points = self.divide_and_conquer(points)
-        #print(polygon_points)
         polygon = [QLineF(polygon_points[i], polygon_points[(i+1) % len(polygon_points)]) for i in range(len(polygon_points))]
 
 
-
-        t4 = time.time()
+        t2 = time.time()
 
         # when passing lines to the display, pass a list of QLineF objects.  Each QLineF
         # object can be created with two QPointF objects corresponding to the endpoints
         self.showHull(polygon,RED)
-        self.showText('Time Elapsed (Convex Hull): {:3.3f} sec'.format(t4-t3))
+        self.showText('Time Elapsed (Convex Hull): {:3.3f} sec'.format(t2-t1))
