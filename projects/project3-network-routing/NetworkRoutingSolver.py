a640fd24801a96b4d065bc3d4ba4527eb83ace18
Max Forsey <maxforsey@Max-Forsey.app.byu.edu>
2024-02-20 19:17:40 -0700

    added dijkstra_array deciding on node_id vs node

--- projects/project3-network-routing/NetworkRoutingSolver.py
+++ projects/project3-network-routing/NetworkRoutingSolver.py
@@ -12,8 +12,27 @@ class NetworkRoutingSolver:
     def initializeNetwork( self, network ):
         assert( type(network) == CS312Graph )
         self.network = network
+        self.shortest_path_to_node = {node.node_id: float('inf') for node in self.network.nodes} #key: node_id (type int), value: cost from src to node (type int)
+        prev = {} # dict of nodes that contain the last edge from previous node to current node in index
 
-    def getShortestPath( self, destIndex ):
+    def dijkstra_array(self,srcIndex):
+        self.shortest_path_to_node[srcIndex] = 0
+        priority_queue = [node for node in self.network.nodes]
+
+        while len(priority_queue) > 0:
+
+            u = min(priority_queue, key=lambda node: self.shortest_path_to_node[node.node_id])
+            priority_queue.remove(u) # remove the found node from the queue
+
+            for edge in u.neighbors: # for each out_degree edge connected to the node
+                new_distance = self.shortest_path_to_node[u.node_id] + edge.length
+                if new_distance < shortest_path_to_node[edge.dest.node_id]: # check if the cost is less than the cost for that node found in dictionary
+                    shortest_path_to_node[edge.dest.node_id] = new_distance # update dictionary
+                    prev[edge.dest.node_id] = u.node_id #update prev with last edge connected to node
+        
+        return prev, self.shortest_path_to_node
+
+    def getShortestPath( self, destIndex):
         self.dest = destIndex
         # TODO: RETURN THE SHORTEST PATH FOR destIndex
         #       INSTEAD OF THE DUMMY SET OF EDGES BELOW
@@ -21,6 +40,23 @@ class NetworkRoutingSolver:
         #       NEED TO USE
         path_edges = []
         total_length = 0
+        
+        key = prev[destIndex]
+
+        while True:
+            try:
+                key = prev[key]
+            except KeyError:
+                break
+            
+
+        # use prev dictionary
+        # start with destIndex key
+            # add the edge.length from value to key to total_length
+            # add the edge to path_edges
+        # move to the value node 
+        # continue until you can't find a key (you find the root node)
+
         node = self.network.nodes[self.source]
         edges_left = 3
         while edges_left > 0:
@@ -34,6 +70,10 @@ class NetworkRoutingSolver:
     def computeShortestPaths( self, srcIndex, use_heap=False ):
         self.source = srcIndex
         t1 = time.time()
+        if use_heap:
+            return None
+        else:
+            dijkstra_array(srcIndex)
         # TODO: RUN DIJKSTRA'S TO DETERMINE SHORTEST PATHS.
         #       ALSO, STORE THE RESULTS FOR THE SUBSEQUENT
         #       CALL TO getShortestPath(dest_index)
